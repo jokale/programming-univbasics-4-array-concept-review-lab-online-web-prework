0b9a9e345ab5419fb41e04c6f28395e4a846657d
Joanna E K <kalemajoanna@gmail.com>
2019-10-31 20:37:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
-def find_element_index(array, value_to_find)
-  # if (array.index(value_to_find) == nil)
-  #   return nil
-  # end
-  index2 = 0   
-  while index2 < array.length do
-    if(array[index2] == value_to_find)
-      puts array.index
-    end
-    index2 += 1 
-  end 
-end 
- 
 # def find_element_index(array, value_to_find)
-#   counter =0 
-#   while array.length > counter do 
-#     p array.index 
-#     counter += 1
-#   end
+#   # if (array.index(value_to_find) == nil)
+#   #   return nil
+#   # end
+#   index2 = 0   
+#   while index2 < array.length do
+#     if(array[index2] == value_to_find)
+#       puts array.index
+#     end
+#     index2 += 1 
 #   end 
+# end 
+ 
+def find_element_index(array, value_to_find)
+  counter =0 
+  while array.length > counter do 
+    p array.index 
+    counter += 1
+  end
+  end 
    
 
 
