86c503ed1a73206f658c8e6bacb9d31b4ff953dc
Joanna E K <kalemajoanna@gmail.com>
2019-10-31 20:34:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,8 +22,15 @@ end
 
 
 def find_max_value(array)
+  max = 5
+  array.length.times do |index|
+    if array[index] > max 
+      max = array[index]
+    end
   end
-
+  return max
+  
+end   
 
 def find_min_value(array)
    min = 10 
